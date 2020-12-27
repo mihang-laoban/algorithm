88a0b03518513a6b853441439f34b38e2f201e8e
tar <mihanglaoban@outlook.com>
2020-12-27 15:43:20 +0800

    show levels

--- alg/dp/dp_test.go
+++ alg/dp/dp_test.go
@@ -368,7 +368,7 @@ func TestFibSingle(t *testing.T) {
 	fmt.Println(cur)
 }
 
-func TestFindLargestSubArray(t *testing.T){
+func TestFindLargestSubArray(t *testing.T) {
 	nums := []int{-1, 4, -2, 3, -2, 3}
 	k := 2
 	// 4 + (3 + -2 + 3) = 8
@@ -377,7 +377,7 @@ func TestFindLargestSubArray(t *testing.T){
 	m := make([][]int, n+1)
 	dp := make([][]int, n+1)
 
-	for i := 0; i < n + 1; i++ {
+	for i := 0; i < n+1; i++ {
 		m[i] = make([]int, k+1)
 		dp[i] = make([]int, k+1)
 	}
@@ -404,8 +404,8 @@ func TestFindLargestSubArray(t *testing.T){
 }
 
 func TestLargestProductSubArr1(t *testing.T) {
-	nums := []int{2,8,-2,4} // [-2, 0, -1] > 0
-//	16
+	nums := []int{2, 8, -2, 4} // [-2, 0, -1] > 0
+	//	16
 	n := len(nums)
 	dp_max := make([]int, n)
 	dp_min := make([]int, n)
@@ -419,15 +419,15 @@ func TestLargestProductSubArr1(t *testing.T) {
 			dp_max[i-1] = dp_min[i-1]
 			dp_min[i-1] = temp
 		}
-		dp_max[i] = tools.Max(nums[i], dp_max[i-1] * nums[i])
-		dp_min[i] = tools.Min(nums[i], dp_min[i-1] * nums[i])
-		res = tools.Max(res, dp_max[i]);
+		dp_max[i] = tools.Max(nums[i], dp_max[i-1]*nums[i])
+		dp_min[i] = tools.Min(nums[i], dp_min[i-1]*nums[i])
+		res = tools.Max(res, dp_max[i])
 	}
 	fmt.Println(res)
 }
 
 func TestLargestProductSubArr2(t *testing.T) {
-	nums := []int{2,8,-2,4} // [-2, 0, -1] > 0
+	nums := []int{2, 8, -2, 4} // [-2, 0, -1] > 0
 	n := len(nums)
 
 	dp := make([][]int, n)
@@ -440,15 +440,53 @@ func TestLargestProductSubArr2(t *testing.T) {
 		dp[i][1] = nums[i]
 	}
 
-	for i := 1; i < n; i ++ {
+	for i := 1; i < n; i++ {
 		// 决策求解
-		dp[i][0] = tools.Max(dp[i - 1][0] * nums[i], tools.Max(nums[i], dp[i - 1][1] * nums[i]))
-		dp[i][1] = tools.Min(dp[i - 1][1] * nums[i], tools.Min(nums[i], dp[i - 1][0] * nums[i]))
+		dp[i][0] = tools.Max(dp[i-1][0]*nums[i], tools.Max(nums[i], dp[i-1][1]*nums[i]))
+		dp[i][1] = tools.Min(dp[i-1][1]*nums[i], tools.Min(nums[i], dp[i-1][0]*nums[i]))
 	}
 
 	res := dp[0][0]
-	for i := 1; i < n; i ++ {
+	for i := 1; i < n; i++ {
 		res = tools.Max(res, dp[i][0])
 	}
+	fmt.Println(res)
+}
+
+/*示例1：
+输入：[3, 3, 5, 0, 0, 3, 1, 4]
+输出：6
+解释：在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3 - 0 = 3 。随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4 - 1 = 3 。
+示例2：
+输入：[1, 2, 3, 4, 5]
+输出：4
+解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4。需要注意的是，你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+示例3：
+输入：[7, 6, 4, 3, 1]
+输出：0
+解释：在这个情况下, 没有交易完成, 所以最大利润为 0。*/
+func TestStock(t *testing.T) {
+	prices := []int{3, 3, 5, 0, 0, 3, 1, 4}
+	length := len(prices)
+	dp := make([][2][3]int, length)
+	// 假设第一天没有买入
+	dp[0][0][0] = 0
+	dp[0][0][1] = 0
+	dp[0][0][2] = 0
+	// 第一天不可能已卖出
+	dp[0][1][0] = -prices[0]
+	dp[0][1][1] = -prices[0]
+	dp[0][1][2] = -prices[0]
+	// 处理后续日期
+	for i := 1; i < length; i++ {
+		dp[i][0][0] = 0
+		dp[i][0][1] = tools.Max(dp[i-1][1][0]+prices[i], dp[i-1][0][1])
+		dp[i][0][2] = tools.Max(dp[i-1][1][1]+prices[i], dp[i-1][0][2])
+		dp[i][1][0] = tools.Max(dp[i-1][0][0]-prices[i], dp[i-1][1][0])
+		dp[i][1][1] = tools.Max(dp[i-1][0][1]-prices[i], dp[i-1][1][1])
+		dp[i][1][2] = 0
+	}
+
+	res := tools.Max(dp[length-1][0][1], dp[length-1][0][2])
 	fmt.Println(res)
 }
