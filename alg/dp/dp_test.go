6f561b016600d25c2c1fa274813fe9b7f1343748
程烨 <chengye1@xiaomi.com>
2020-12-29 10:32:04 +0800

    works

--- alg/dp/dp_test.go
+++ alg/dp/dp_test.go
@@ -3,7 +3,6 @@ package dp
 import (
 	. "dp/tools"
 	"fmt"
-	"math"
 	"testing"
 )
 
@@ -551,60 +550,4 @@ func TestLargestProductSubArr2(t *testing.T) {
 		res = Max(res, dp[i][0])
 	}
 	fmt.Println(res)
-}
-
-
-/*给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
-
-设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
-
-注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
-
- 
-
-示例 1：
-
-输入：k = 2, prices = [2,4,1]
-输出：2
-解释：在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。
-示例 2：
-
-输入：k = 2, prices = [3,2,6,5,0,3]
-输出：7
-解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
-随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 。
- 
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
-
-func TestMaxProfitK(t *testing.T) {
-	//fmt.Println(maxProfitK(2, []int{3,2,6,5,0,3}))
-	fmt.Println(maxProfitK(2, []int{3, 2, 6, 5, 0, 3}))
-}
-
-func maxProfitK(k int, prices []int) int {
-	n := len(prices)
-	if n == 0 {
-		return 0
-	}
-
-	k = min(k, n/2)
-	buy := make([]int, k+1)
-	sell := make([]int, k+1)
-	buy[0] = -prices[0]
-	for i := 1; i <= k; i++ {
-		buy[i] = math.MinInt64 / 2
-		sell[i] = math.MinInt64 / 2
-	}
-
-	for i := 1; i < n; i++ {
-		buy[0] = max(buy[0], sell[0]-prices[i])
-		for j := 1; j <= k; j++ {
-			buy[j] = max(buy[j], sell[j]-prices[i])
-			sell[j] = max(sell[j], buy[j-1]+prices[i])
-		}
-	}
-	return max(sell...)
 }
--- alg/dp/stock_test.go
+++ alg/dp/stock_test.go
@@ -3,6 +3,7 @@ package dp
 import (
 	. "dp/tools"
 	"fmt"
+	"math"
 	"testing"
 )
 
@@ -176,4 +177,60 @@ func stockGreed(prices []int) (ans int){
 		ans += Max(0, prices[i]-prices[i-1])
 	}
 	return
+}
+
+
+/*给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+ 
+
+示例 1：
+
+输入：k = 2, prices = [2,4,1]
+输出：2
+解释：在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。
+示例 2：
+
+输入：k = 2, prices = [3,2,6,5,0,3]
+输出：7
+解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
+随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 。
+ 
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+// Q20
+func TestMaxProfitK(t *testing.T) {
+	fmt.Println(maxProfitK1(2, []int{3, 2, 6, 5, 0, 3}))
+	//fmt.Println(maxProfitK1(2, []int{3, 3, 5, 0, 0, 3, 1, 4}))
+}
+
+func maxProfitK1(k int, prices []int) int {
+	n := len(prices)
+	if n == 0 {
+		return 0
+	}
+
+	k = Mini(k, n/2)
+	buy := make([]int, k+1)
+	sell := make([]int, k+1)
+	buy[0] = -prices[0]
+	for i := 1; i <= k; i++ {
+		buy[i] = math.MinInt64 / 2
+		sell[i] = math.MinInt64 / 2
+	}
+
+	for i := 1; i < n; i++ {
+		buy[0] = Maxi(buy[0], sell[0]-prices[i])
+		for j := 1; j <= k; j++ {
+			buy[j] = Maxi(buy[j], sell[j]-prices[i])
+			sell[j] = Maxi(sell[j], buy[j-1]+prices[i])
+		}
+	}
+	return Maxi(sell...)
 }
