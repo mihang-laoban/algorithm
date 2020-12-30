0fa8649f2b42f7fefc65c1cb39a3e069e5b27c3f
程烨 <chengye1@xiaomi.com>
2020-12-30 19:05:01 +0800

    tree

--- alg/dp/dp_test.go
+++ alg/dp/dp_test.go
@@ -40,9 +40,15 @@ w = [3, 2, 1], v = [5, 2, 3]
 */
 // Q2
 func TestPackage(t *testing.T) {
-	weights, values := []int{0, 3, 2, 1}, []int{0, 5, 2, 3}
+	weights, values := []int{3, 2, 1}, []int{5, 2, 3}
+	weights = append([]int{0}, weights...)
+	values = append([]int{0}, values...)
 	totalWeight, totalCount := 5, 3
-	packImproved(weights, values, totalWeight, totalCount)
+
+	pk(weights, values, totalWeight, totalCount)
+}
+
+func pk(weights []int, values []int, totalWeight int, totalCount int) {
 }
 
 func pack(weights []int, values []int, totalWeight int, totalCount int) {
@@ -93,6 +99,8 @@ func packImproved(weights []int, values []int, totalWeight int, totalCount int) 
 }
 
 // Q3
+//问题：给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+// 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
 func TestDp(t *testing.T) {
 	str := "aaa"
 	largestSubArr(str)
@@ -137,6 +145,7 @@ func TestFindLargestArrSum(t *testing.T) {
 }
 
 // Q5
+//问题：给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000。
 func TestFindLargestSubSeq(t *testing.T) {
 	str := "asssasms" // 5
 	FindLargestSubSeq(str)
@@ -163,6 +172,9 @@ func FindLargestSubSeq(str string) {
 }
 
 // Q6
+//问题：给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+// 若这两个字符串没有公共子序列，则返回 0。
+// 其中：1 ≤ text1.length ≤ 1000；1 ≤ text2.length ≤ 1000；输入的字符串只含有小写英文字符。
 func TestFindLongestSharedSubSeq(t *testing.T) {
 	text1 := "abcde"
 	text2 := "ade"
@@ -348,7 +360,11 @@ func findContinuous(arr []int) {
 	fmt.Println(res)
 }
 
-/*示例：
+/*
+问题：给定一个无序的整数数组 nums，找到其中最长上升子序列的长度（Longest Increasing Subsequence，LIS）。
+附加条件是：可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可；你算法的时间复杂度应该为 O(n2)。
+
+示例：
 
 输入: nums = [10, 9, 1, 5, 2, 6, 66, 18]
 输出: 4
