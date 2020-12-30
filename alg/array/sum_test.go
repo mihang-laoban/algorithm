5d66a9149663d2415ae9cf5a0e9f1f775d49bba8
程烨 <chengye1@xiaomi.com>
2020-12-30 14:49:11 +0800

    tree

--- alg/array/sum_test.go
+++ alg/array/sum_test.go
@@ -10,8 +10,6 @@ import (
 
 注意：答案中不可以包含重复的三元组。
 
-
-
 示例：
 
 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
@@ -27,7 +25,7 @@ import (
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
 func TestThree(t *testing.T) {
 	nums := []int{-1, 0, 1, 2, -1, -4}
-	res := threesome(nums)
+	res := three(nums)
 	fmt.Println(res)
 }
 
@@ -94,6 +92,10 @@ func threesome(nums []int) (res [][]int) {
 	return
 }
 
+func three(nums []int) (res [][]int) {
+	return
+}
+
 /*给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
@@ -117,12 +119,5 @@ func TestTwoSum(t *testing.T) {
 }
 
 func twoSum(nums []int, target int) []int {
-	tmp := map[int]int{}
-	for k, v := range nums {
-		if p, ok := tmp[target-v]; ok {
-			return []int{p, k}
-		}
-		tmp[v] = k
-	}
 	return nil
 }
