d98c0627ae95e00528ff1e3061e34ae7e9f54f86
tar <mihanglaoban@outlook.com>
2021-01-01 21:41:48 +0800

    show levels

--- ds/Stack_test.go
+++ ds/Stack_test.go
@@ -1,13 +1,62 @@
 package ds
 
+import (
+	"fmt"
+	"testing"
+)
+
 // first in last out
+
 type Stack struct {
+	data []interface{}
+	size int
 }
 
 type StackOperations interface {
-	push()
-	pop()
-	peek()
-	empty()
-	search()
+	push(interface{})
+	pop() interface{}
+	peek() interface{}
+	empty() bool
+	search(interface{}) int
+}
+
+func (stack *Stack) push(val interface{}) {
+	stack.data = append(stack.data, val)
+	stack.size++
+}
+
+func (stack *Stack) pop() (last interface{}) {
+	if stack.size > 0 {
+		last = stack.data[stack.size-1]
+		stack.data = stack.data[:stack.size-1]
+		stack.size--
+		return
+	}
+	return -1
+}
+
+func (stack *Stack) peek() interface{} {
+	return stack.data[stack.size-1]
+}
+
+func (stack *Stack) empty() bool {
+	if stack.size == 0 && len(stack.data) == 0 {
+		return true
+	}
+	return false
+}
+
+func (stack *Stack) search(val interface{}) int {
+	for k, v := range stack.data {
+		if v == val {
+			return k
+		}
+	}
+	return -1
+}
+
+func Test(t *testing.T) {
+	stack := Stack{}
+	fmt.Println(stack.empty())
+	stack.push(1)
 }
