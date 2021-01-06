77dc5d188ec1a7a4ce2e02bf4055a08575f8aaa9
tar <mihanglaoban@outlook.com>
2021-01-06 23:01:11 +0800

    show levels

--- alg/tree/tree_test.go
+++ alg/tree/tree_test.go
@@ -154,10 +154,76 @@ func TestGo(t *testing.T) {
 	fmt.Println(checker(root))
 }
 
-func Test(t *testing.T) {
+func TestTreeAndArray(t *testing.T) {
 	arr := []interface{}{5, 1, 4, nil, nil, 3, 6}
 	fmt.Println(arr)
 	root := ArrayToTree(arr)
 	res := TreeToArray(root)
 	fmt.Println(res)
+}
+
+/*给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+假设一个二叉搜索树具有如下特征：
+
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+示例 1:
+
+输入:
+  2
+ / \
+1   3
+输出: true
+示例 2:
+
+输入:
+    5
+   / \
+  1   4
+     / \
+    3   6
+输出: false
+解释: 输入为: [5,1,4,null,null,3,6]。
+     根节点的值为 5 ，但是其右子节点值为 4 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+func TestValidateBinaryTree(t *testing.T) {
+	nums := []interface{}{5, 1, 4, nil, nil, 3, 6}
+	root := ArrayToTree(nums)
+	res := helper(root, math.MinInt64, math.MaxInt64)
+	fmt.Println(res)
+}
+
+func helper(root *TreeNode, lower, upper int) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val <= lower || root.Val >= upper {
+		return false
+	}
+	return helper(root.Left, lower, root.Val) && helper(root.Right, root.Val, upper)
+}
+
+func isValidBST(root *TreeNode) bool {
+	stack := []*TreeNode{}
+	inorder := math.MinInt64
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if root.Val <= inorder {
+			return false
+		}
+		inorder = root.Val
+		root = root.Right
+	}
+	return true
 }
