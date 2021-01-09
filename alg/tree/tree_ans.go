d2b128bbbba59db8998aff830fdf4347adfb48be
tar <mihanglaoban@outlook.com>
2021-01-09 11:56:20 +0800

    show levels

--- alg/tree/tree_ans.go
+++ alg/tree/tree_ans.go
@@ -219,6 +219,7 @@ func PostOrderLoop(root *TreeNode) (res []int) {
 	stack := []*TreeNode{}
 	for len(stack) != 0 || root != nil {
 		if root != nil {
+			// 新元素添加到队列头
 			res = append([]int{root.Val}, res...)
 			if root.Left != nil {
 				stack = append(stack, root.Left)
