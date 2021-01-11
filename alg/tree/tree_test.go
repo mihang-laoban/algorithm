5ca1c332b156740faba72aa53ae9d7dc15553910
程烨 <chengye1@xiaomi.com>
2021-01-11 14:06:08 +0800

    request data from mock ok

--- alg/tree/tree_test.go
+++ alg/tree/tree_test.go
@@ -493,6 +493,51 @@ func FindContentChildren(g, s []int) (count int) {
 	return count
 }
 
-//https://leetcode-cn.com/problems/sqrtx/
+/*
+实现 int sqrt(int x) 函数。
+
+计算并返回 x 的平方根，其中 x 是非负整数。
+
+由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+示例 1:
+输入: 4
+输出: 2
+
+示例 2:
+输入: 8
+输出: 2
+说明: 8 的平方根是 2.82842...,
+     由于返回类型是整数，小数部分将被舍去。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sqrtx
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func TestSqrt(t *testing.T) {
+	fmt.Println(mySqrt(8))
+}
+
+func mySqrt(x int) int {
+	// 记录左右边界
+	l, r := 0, x
+	ans := -1
+	// 如果左边大于右边
+	for l <= r {
+		// 查找中位数
+		mid := l + (r-l)/2
+		// 如果中位数的平方小于目标，则更新结果，然后向上查找
+		if mid*mid > x {
+			r = mid - 1
+		} else {
+			// 如果中位数的平方大于目标，向下查找
+			ans = mid
+			l = mid + 1
+		}
+	}
+	return ans
+}
+
 //https://leetcode-cn.com/problems/valid-perfect-square/
 //https://leetcode-cn.com/problems/search-rotate-array-lcci/
