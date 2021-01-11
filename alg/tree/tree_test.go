4743aae77b31c2ff24bb83e0445e905fe9fc84b7
程烨 <chengye1@xiaomi.com>
2021-01-11 19:22:19 +0800

    request data from mock ok

--- alg/tree/tree_test.go
+++ alg/tree/tree_test.go
@@ -446,5 +446,55 @@ func IslandBfs(grid [][]string) int {
 	return count
 }
 
-//https://leetcode-cn.com/problems/valid-perfect-square/
+/*
+给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
+
+说明：不要使用任何内置的库函数，如  sqrt。
+
+示例 1：
+
+输入：16
+输出：True
+示例 2：
+
+输入：14
+输出：False
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-perfect-square
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
 //https://leetcode-cn.com/problems/search-rotate-array-lcci/
+
+func TestPerfectSqure(t *testing.T) {
+	fmt.Println(isPerfectSquare(16))
+	fmt.Println(isPerfectSquareOld(16))
+}
+
+func isPerfectSquareOld(num int) interface{} {
+	if num < 2 {
+		return true
+	}
+	l, r := 0, num
+	for l <= r {
+		mid := l + (r-l)/2
+		if mid*mid == num {
+			return true
+		} else if mid*mid > num {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return false
+}
+
+func isPerfectSquare(n int) interface{} {
+	start := 1
+	for n > 0 {
+		n -= start
+		start += 2
+	}
+	return n == 0
+}
