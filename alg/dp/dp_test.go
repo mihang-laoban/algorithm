661e2994560a489cd0a269161769345eace62e06
程烨 <chengye1@xiaomi.com>
2021-01-13 13:36:43 +0800

    request data from mock ok

--- alg/dp/dp_test.go
+++ alg/dp/dp_test.go
@@ -245,7 +245,7 @@ func TestClimbMinCost(t *testing.T) {
 	cost := []int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}
 	size := len(cost)
 	pre, cur := 0, 0
-	for i := 2; i <= size; i++ {
+	for i := 2; i < size+1; i++ {
 		tmp := Min(cur+cost[i-1], pre+cost[i-2])
 		pre = cur
 		cur = tmp
