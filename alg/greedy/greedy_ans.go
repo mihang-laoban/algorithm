4983fbc5a9e1fb6a78fb1539b7118f2169b8825c
程烨 <chengye1@xiaomi.com>
2021-01-14 13:02:54 +0800

    request data from mock ok

--- alg/greedy/greedy_ans.go
+++ alg/greedy/greedy_ans.go
@@ -7,8 +7,7 @@ func FindContentChildren(g, s []int) (count int) {
 	sort.Ints(s)
 	// 查找饼干
 	for j := 0; count < len(g) && j < len(s); j++ {
-		// 如果饼干能满足当前孩子，就看下一个孩子
-		// 如果不能就看下一个饼干
+		// 如果饼干能满足当前孩子，就看下一个孩子，如果不能就看下一个饼干
 		if g[count] <= s[j] {
 			count++
 		}
