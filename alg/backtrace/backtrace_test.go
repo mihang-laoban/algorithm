7a447d73868291dc971027576199f7a7529834af
程烨 <chengye1@xiaomi.com>
2021-01-15 12:08:56 +0800

    request data from mock ok

--- alg/backtrace/backtrace_test.go
+++ alg/backtrace/backtrace_test.go
@@ -315,3 +315,89 @@ func WordSearch2(board [][]byte, word string) bool {
 
 	return false
 }
+
+/*给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words，找出所有同时在二维网格和字典中出现的单词。
+单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使用。
+
+Input:
+board = [
+["o","a","a","n"],
+["e","t","a","e"],
+["i","h","k","r"],
+["i","f","l","v"]],
+words = ["oath","pea","eat","rain"]
+Output: ["eat","oath"]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/word-search-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+func TestWordSearchII(t *testing.T) {
+	board := [][]byte{
+		[]byte{'o', 'a', 'a', 'n'},
+		[]byte{'e', 't', 'a', 'e'},
+		[]byte{'i', 'h', 'k', 'r'},
+		[]byte{'i', 'f', 'l', 'v'},
+	}
+	words := []string{"oath", "pea", "eat", "rain"}
+	fmt.Println(findWords(board, words))
+}
+
+type TrieNode struct {
+	next [26]*TrieNode
+	word string
+}
+
+func build(words []string) *TrieNode {
+	root := &TrieNode{}
+	for _, word := range words {
+		p := root
+		for _, w := range word {
+			if p.next[w-'a'] == nil {
+				p.next[w-'a'] = &TrieNode{}
+			}
+			p = p.next[w-'a']
+		}
+		p.word = word
+	}
+	return root
+}
+
+func findWords(board [][]byte, words []string) []string {
+	res := []string{}
+	root := build(words)
+	var dfs func(int, int, *TrieNode)
+
+	dfs = func(i, j int, root *TrieNode) {
+		c := board[i][j]
+		if c == '#' || root.next[c-'a'] == nil {
+			return
+		}
+		root = root.next[c-'a']
+		if root.word != "" {
+			res = append(res, root.word)
+			root.word = ""
+		}
+		board[i][j] = '#'
+		if i > 0 {
+			dfs(i-1, j, root)
+		}
+		if j > 0 {
+			dfs(i, j-1, root)
+		}
+		if i < len(board)-1 {
+			dfs(i+1, j, root)
+		}
+		if j < len(board[0])-1 {
+			dfs(i, j+1, root)
+		}
+		board[i][j] = c
+	}
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			dfs(i, j, root)
+		}
+	}
+	return res
+}
