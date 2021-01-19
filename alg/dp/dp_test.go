ff2a8cd711a1f09c23884994ef71e91cc60e74ff
程烨 <chengye1@xiaomi.com>
2021-01-19 13:30:13 +0800

    request data from mock ok

--- alg/dp/dp_test.go
+++ alg/dp/dp_test.go
@@ -1,6 +1,7 @@
 package dp
 
 import (
+	. "dp/ds/tree"
 	. "dp/tools"
 	"fmt"
 	"testing"
@@ -305,6 +306,65 @@ func TestGenBrackets(t *testing.T) {
 	n := 3
 	fmt.Println(GenerateParenthesis(n))
 	fmt.Println(DpGenerateParenthesis(n))
+}
+
+/*在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+
+计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+
+示例 1:
+
+输入: [3,2,3,null,3,null,1]
+
+  3
+ / \
+2   3
+ \   \
+  3   1
+
+输出: 7
+解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+示例 2:
+
+输入: [3,4,5,1,3,null,1]
+
+     3
+    / \
+   4   5
+  / \   \
+ 1   3   1
+
+输出: 9
+解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/house-robber-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+func TestRobTree(t *testing.T) {
+	root1 := ArrayToTree([]interface{}{3, 2, 3, nil, 3, nil, 1})
+	root2 := ArrayToTree([]interface{}{3, 4, 5, 1, 3, nil, 1})
+	fmt.Println(RobTree(root1))
+	fmt.Println(RobTree(root2))
+}
+
+func RobTree(root *TreeNode) interface{} {
+	y, n := _robTree(root)
+	return Max(y, n)
+}
+
+func _robTree(root *TreeNode) (n int, y int) {
+	// 遍历到底，无节点可偷
+	if root == nil {
+		return 0, 0
+	}
+	leftN, leftY := _robTree(root.Left)
+	rightN, rightY := _robTree(root.Right)
+	//当前节点选择不偷：当前节点能偷到的最大钱数 = 左孩子能偷到的钱 + 右孩子能偷到的钱
+	n = Max(leftN, leftY) + Max(rightN, rightY)
+	//当前节点选择偷：当前节点能偷到的最大钱数 = 左孩子选择自己不偷时能得到的钱 + 右孩子选择不偷时能得到的钱 + 当前节点的钱数
+	y = leftN + rightN + root.Val
+	return n, y
 }
 
 // https://leetcode-cn.com/problems/unique-paths/
