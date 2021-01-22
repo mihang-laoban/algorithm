88712e1bbe3a1230f74386281acec73daef8d382
程烨 <chengye1@xiaomi.com>
2021-01-22 17:40:58 +0800

    request data from mock ok

--- alg/tree/tree_test.go
+++ alg/tree/tree_test.go
@@ -622,30 +622,30 @@ func recoverT(root *TreeNode) {
 /*翻转一棵二叉树。
 示例：
 输入：
-     4
-   /   \
-  2     7
- / \   / \
-1   3 6   9
+    4
+   / \
+  2   7
+ / \ / \
+1  3 6  9
 输出：
 
-     4
-   /   \
-  7     2
- / \   / \
-9   6 3   1
+	4
+   / \
+  7   2
+ / \ / \
+9  6 3  1
 
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/invert-binary-tree
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
 
 func TestReverseTree(t *testing.T) {
-	root := ArrayToTree([]interface{}{4,2,7,1,3,6,9})
+	root := ArrayToTree([]interface{}{4, 2, 7, 1, 3, 6, 9})
 	root = ReverseTreeR(root)
 	root = ReverseTreeL(root)
 	fmt.Println(BFS(root))
 }
 
 func TestTree(t *testing.T) {
-	
+
 }
