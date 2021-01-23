247ae8f94fd8a1cdd48afa4483683ce86e2c9852
tar <mihanglaoban@outlook.com>
2021-01-23 22:37:46 +0800

    show levels

--- alg/linkedList/linkedList_ans.go
+++ alg/linkedList/linkedList_ans.go
@@ -38,3 +38,48 @@ func MergeLinkedListR(l1, l2 *NodeInt) *NodeInt {
 		return l2
 	}
 }
+
+func SwapPairsR(head *NodeInt) *NodeInt {
+	// 如果头节点或者下一个节点尾空，则无法交换
+	if head == nil || head.Next == nil {
+		return head
+	}
+	// 存储第二个节点
+	newHead := head.Next
+	// 第二个节点指向第三个节点交换以后的结果
+	head.Next = SwapPairsR(newHead.Next)
+	// 第二个节点成为新的头节点
+	newHead.Next = head
+	return newHead
+
+	/*	if head == nil || head.Next == nil {
+			return head
+		}
+		third := SwapPairsR(head.Next.Next)
+		second := head.Next
+		head.Next = third
+		second.Next = head
+		return second*/
+}
+func SwapPairsL(head *NodeInt) *NodeInt {
+	dummyHead := &NodeInt{head, 0}
+	cur := dummyHead
+	/*
+		c, 1, 2, 3, 4
+		f = 1
+		s = 2
+		c.next = s
+		f.next = s.next
+		s.next = f
+		c = f
+	*/
+	for cur.Next != nil && cur.Next.Next != nil {
+		first := cur.Next
+		second := cur.Next.Next
+		cur.Next = second
+		first.Next = second.Next
+		second.Next = first
+		cur = first
+	}
+	return dummyHead.Next
+}
