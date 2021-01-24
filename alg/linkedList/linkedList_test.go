067af4f3753e6c001176916df935f72519abfe7e
tar <mihanglaoban@outlook.com>
2021-01-24 16:46:47 +0800

    show levels

--- alg/linkedList/linkedList_test.go
+++ alg/linkedList/linkedList_test.go
@@ -91,11 +91,44 @@ func Test(t *testing.T) {
 	fmt.Println(LinkedListToArray(ReverseKGroup(head, 3)))
 }
 
+/*反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+说明:
+1 ≤ m ≤ n ≤ 链表长度。
+
+示例:
+输入: 1->2->3->4->5->NULL, m = 2, n = 4
+输出: 1->4->3->2->5->NULL
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-linked-list-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
 func TestReverseLinkedList(t *testing.T) {
 	head := ArrayToLinkedList([]int{1, 2, 3, 4, 5})
 	//fmt.Println(LinkedListToArray(ReverseListR(head)))
-	fmt.Println(LinkedListToArray(ReverseKListR(head, 3)))
+	//fmt.Println(LinkedListToArray(ReverseKListR(head, 3)))
 	//fmt.Println(LinkedListToArray(ReverseListBetweenR(head, 2, 4)))
+	fmt.Println(LinkedListToArray(ReverseListBetweenL(head, 2, 4)))
+}
+
+func ReverseListBetweenL(head *ListNode, m, n int) *ListNode {
+	dummy := &ListNode{Val: -1}
+	dummy.Next = head
+	step, guard, cur := 0, dummy, dummy.Next
+	for step < m-1 {
+		guard, cur = guard.Next, cur.Next
+		step++
+	}
+
+	for i := 0; i < n-m; i++ {
+		removed := cur.Next
+		cur.Next = cur.Next.Next
+
+		removed.Next = guard.Next
+		guard.Next = removed
+	}
+
+	return dummy.Next
 }
 
 func ReverseListBetweenR(head *ListNode, m, n int) *ListNode {
@@ -125,3 +158,19 @@ func ReverseKListR(head *ListNode, n int) *ListNode {
 
 	return _reverseFirstK(head, n)
 }
+
+/*
+用一个 非空 单链表来表示一个非负整数，然后将这个整数加一。
+
+你可以假设这个整数除了 0 本身，没有任何前导的 0。
+
+这个整数的各个数位按照 高位在链表头部、低位在链表尾部 的顺序排列。
+
+示例:
+
+输入: [1,2,3]
+输出: [1,2,4]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/plus-one-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
