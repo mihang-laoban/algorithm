78c9e446b004430794a996a127c70fe710ef7451
tar <mihanglaoban@outlook.com>
2021-02-01 01:09:45 +0800

    show levels

--- alg/linkedList/linkedList_test.go
+++ alg/linkedList/linkedList_test.go
@@ -735,3 +735,73 @@ func RotateRight(head *ListNode, k int) *ListNode {
 	head, newHead.Next = newHead.Next, nil
 	return head
 }
+
+/*给定一个头结点为 root 的链表, 编写一个函数以将链表分隔为 k 个连续的部分。
+每部分的长度应该尽可能的相等: 任意两部分的长度差距不能超过 1，也就是说可能有些部分为 null。
+这k个部分应该按照在链表中出现的顺序进行输出，并且排在前面的部分的长度应该大于或等于后面的长度。
+返回一个符合上述规则的链表的列表。
+
+举例： 1->2->3->4, k = 5 // 5 结果 [ [1], [2], [3], [4], null ]
+
+示例 1：
+输入:
+root = [1, 2, 3], k = 5
+输出: [[1],[2],[3],[],[]]
+解释:
+输入输出各部分都应该是链表，而不是数组。
+例如, 输入的结点 root 的 val= 1, root.next.val = 2, \root.next.next.val = 3, 且 root.next.next.next = null。
+第一个输出 output[0] 是 output[0].val = 1, output[0].next = null。
+最后一个元素 output[4] 为 null, 它代表了最后一个部分为空链表。
+
+示例 2：
+输入:
+root = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], k = 3
+输出: [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]]
+解释:
+输入被分成了几个连续的部分，并且每部分的长度相差不超过1.前面部分的长度大于等于后面部分的长度。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/split-linked-list-in-parts
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+func TestSplitListToParts(t *testing.T) {
+	head1 := ArrayToLinkedList([]int{1, 2, 3})
+	head2 := ArrayToLinkedList([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	heads1 := SplitListToParts(head1, 5)
+	heads2 := SplitListToParts(head2, 3)
+	for i := 0; i < len(heads1); i++ {
+		fmt.Println(LinkedListToArray(heads1[i]))
+	}
+	for i := 0; i < len(heads2); i++ {
+		fmt.Println(LinkedListToArray(heads2[i]))
+	}
+}
+
+func SplitListToParts(root *ListNode, k int) []*ListNode {
+	cur, size := root, 0
+	for cur != nil {
+		cur = cur.Next
+		size++
+	}
+	width, rem, ans := size/k, size%k, make([]*ListNode, k)
+	cur = root
+	for i := 0; i < k; i++ {
+		head, good := cur, 0
+		if i < rem {
+			good = 1
+		} else {
+			good = 0
+		}
+		for j := 0; j < width+good-1; j++ {
+			if cur != nil {
+				cur = cur.Next
+			}
+		}
+		if cur != nil {
+			prev := cur
+			cur, prev.Next = cur.Next, nil
+		}
+		ans[i] = head
+	}
+	return ans
+}
