549d4adc785fe52a6a49483e8d2f67fd9629a244
tar <mihanglaoban@outlook.com>
2021-03-08 22:52:14 +0800

    show levels

--- alg/backtrace/backtrace_test.go
+++ alg/backtrace/backtrace_test.go
@@ -117,9 +117,9 @@ func Phone(str string) interface{} {
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
 
 func TestNQueue(t *testing.T) {
-	//res := SolveNQueens(4)
+	res := SolveNQueens(4)
 	//res := SolveNQueen(4)
-	res := SolveNQueens2(4)
+	//res := SolveNQueens2(4)
 
 	for _, v := range res {
 		for _, s := range v {
