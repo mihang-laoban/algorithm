7570e912ecc89fb186731f49cf5e14b0eec97160
程烨 <chengye1@xiaomi.com>
2021-03-11 16:34:59 +0800

    request data from mock ok

--- alg/tree/tree_test.go
+++ alg/tree/tree_test.go
@@ -1371,3 +1371,69 @@ func (uf unionFind) find(x int) int {
 func (uf unionFind) union(from, to int) {
 	uf.ancestor[uf.find(from)] = uf.find(to)
 }
+
+/*
+给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+
+例如：
+给定二叉树 [3,9,20,null,null,15,7],
+
+  3
+ / \
+9  20
+  /  \
+15    7
+返回锯齿形层序遍历如下：
+
+[
+[3],
+[20,9],
+[15,7]
+]
+
+    1
+   / \
+  2   3
+ /     \
+4       5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+func TestZigzagLevelOrder(t *testing.T) {
+	root := ArrayToTree([]interface{}{1, 2, 3, 4, nil, nil, 5})
+	arr := ZigzagLevelOrder(root)
+	fmt.Println(arr)
+}
+
+func ZigzagLevelOrder(root *TreeNode) (ans [][]int) {
+	if root == nil {
+		return
+	}
+	queue := []*TreeNode{root}
+	for i := 0; len(queue) > 0; i++ {
+		level := []int{}
+		q := queue
+		queue = nil
+		for _, node := range q {
+			level = append(level, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		if i&1 == 1 {
+			l, r := 0, len(level)-1
+			for l < r {
+				level[l], level[r] = level[r], level[l]
+				l++
+				r--
+			}
+		}
+		ans = append(ans, level)
+	}
+	return
+}
