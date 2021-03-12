750c2b0bfe6b9655774b98c98058a596bbe9f790
程烨 <chengye1@xiaomi.com>
2021-03-12 19:36:45 +0800

    request data from mock ok

--- alg/newCoder/code_test.go
+++ alg/newCoder/code_test.go
@@ -3,6 +3,7 @@ package newCoder
 import (
 	. "dp/ds"
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -140,4 +141,38 @@ func (this *MyQueue) Peek() int {
 /** Returns whether the queue is empty. */
 func (this *MyQueue) Empty() bool {
 	return len(this.inStack) == 0 && len(this.outStack) == 0
+}
+
+/*给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+
+提示：
+num1 和num2 的长度都小于 5100
+num1 和num2 都只包含数字 0-9
+num1 和num2 都不包含任何前导零
+你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-strings
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+func TestAddStrings(t *testing.T) {
+	fmt.Println(AddStrings("512", "357"))
+
+}
+
+func AddStrings(num1 string, num2 string) string {
+	ans, add := "", 0
+	for i, j := len(num1)-1, len(num2)-1; i >= 0 || j >= 0 || add != 0; i, j = i-1, j-1 {
+		var x, y int
+		if i >= 0 {
+			x = int(num1[i] - '0')
+		}
+		if j >= 0 {
+			y = int(num2[j] - '0')
+		}
+		result := x + y + add
+		ans = strconv.Itoa(result%10) + ans
+		add = result / 10
+	}
+	return ans
 }
