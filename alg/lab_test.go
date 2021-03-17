1eb24185b3fd2430550490d83f54bb8d1fbdc2bb
tar <mihanglaoban@outlook.com>
2021-03-17 21:42:41 +0800

    show levels

--- alg/lab_test.go
+++ alg/lab_test.go
@@ -219,17 +219,8 @@ func BenchmarkStringBuilder(b *testing.B) {
 	b.StopTimer()
 }
 
-<<<<<<< HEAD
 func TestRemoveDuplicate(t *testing.T) {
 	str := "asbb j   pq[we"
 	fmt.Println(str)
-=======
-func TestSortString(t *testing.T) {
-	str := []string{"z", "c", "d", "aasds"}
-	sort.Strings(str)
-	fmt.Println(str)
-	fmt.Println(str)
-	fmt.Println(str)
 	fmt.Println(io.EOF)
->>>>>>> e3f01e12654a440f6b5704dc6606dfdde27605e7
 }
