75c89a7f92498a4dcc54aef974a3e7598dadb5e0
程烨 <chengye1@xiaomi.com>
2021-03-25 12:03:35 +0800

    request data from mock ok

--- main/main.go
+++ main/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func ReadLine() string {
@@ -14,9 +16,40 @@ func ReadLine() string {
 	return r
 }
 
+func ReadInt() (res [][]int) {
+	reader := bufio.NewScanner(os.Stdin)
+	for reader.Scan() {
+		curNums := []int{}
+		str := reader.Text()
+		trimmed := strings.TrimSpace(str)
+		strArr := strings.Split(trimmed, " ")
+		for _, value := range strArr {
+			curInt, _ := strconv.Atoi(value)
+			curNums = append(curNums, curInt)
+		}
+		res = append(res, curNums)
+		fmt.Println(res)
+	}
+	return
+}
+
+func ReadInt2() (res []int) {
+	reader := bufio.NewScanner(os.Stdin)
+	for reader.Scan() {
+		str := reader.Text()
+		trimmed := strings.TrimSpace(str)
+		strArr := strings.Split(trimmed, " ")
+		for _, value := range strArr {
+			curInt, _ := strconv.Atoi(value)
+			res = append(res, curInt)
+		}
+	}
+	return
+}
+
 func Start() {
-	fmt.Println(ReadLine())
-	fmt.Println(ReadLine())
+	//fmt.Println(ReadLine())
+	ReadInt2()
 }
 
 func main() {
