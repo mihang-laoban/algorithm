ace6c7d4c7dfb7aa405266aca0245cc128c2de5e
程烨 <chengye1@xiaomi.com>
2021-04-06 17:39:10 +0800

    request data from mock ok

--- alg/slidingWindow/slidingWindow_test.go
+++ alg/slidingWindow/slidingWindow_test.go
@@ -219,3 +219,50 @@ func CheckInclusion(s1, s2 string) bool {
 	}
 	return false
 }
+
+/*
+给你一个仅由大写英文字母组成的字符串，你可以将任意位置上的字符替换成另外的字符，总共可最多替换 k 次。在执行上述操作后，找到包含重复字母的最长子串的长度。
+
+注意：字符串长度 和 k 不会超过 104。
+
+
+
+示例 1：
+
+输入：s = "ABAB", k = 2
+输出：4
+解释：用两个'A'替换为两个'B',反之亦然。
+示例 2：
+
+输入：s = "AABABBA", k = 1
+输出：4
+解释：
+将中间的一个'A'替换为'B',字符串变为 "AABBBBA"。
+子串 "BBBB" 有最长重复字母, 答案为 4。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-repeating-character-replacement
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func TestCharacterReplacement(t *testing.T) {
+	fmt.Println(CharacterReplacement("AABCABBB", 2))
+	fmt.Println(CharacterReplacement("ABAB", 2))
+	fmt.Println(CharacterReplacement("AABABBA", 1))
+}
+
+func CharacterReplacement(s string, k int) int {
+	var memo [26]int
+	size := len(s)
+	max := math.MinInt64
+	l := 0
+	for r := 0; r < size; r++ {
+		memo[s[r]-'A']++
+		max = Max(max, memo[s[r]-'A'])
+		if r-l+1 > max+k {
+			memo[s[l]-'A']--
+			l++
+		}
+	}
+	return size - l
+}
