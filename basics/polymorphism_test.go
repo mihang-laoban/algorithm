2f3a38f967fe7cd3a60bb10b84ebc2ceb53c485a
chengye <mihanglaoban@outlook.com>
2021-04-26 17:00:42 +0800

    four cards

--- basics/polymorphism_test.go
+++ basics/polymorphism_test.go
@@ -1,8 +1,10 @@
 package basics
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"runtime"
 	"testing"
 )
 
@@ -111,7 +113,7 @@ type User struct {
 	Age  int
 }
 
-func (this User) Call() {
+func (this User) Call21() {
 	fmt.Println("user is called ...")
 	fmt.Printf("%v\n", this)
 }
@@ -135,7 +137,7 @@ func DoFieldAndMethod(input interface{}) {
 	}
 
 	for i := 0; i < inputType.NumMethod(); i++ {
-		fmt.Println()
+		fmt.Println(0)
 		m := inputType.Method(i)
 		fmt.Printf("%s: %v\n", m.Name, m.Type)
 	}
@@ -146,7 +148,7 @@ func DoFieldAndMethod(input interface{}) {
 
 type resume struct {
 	Name string `info:"name" doc:"我的名字"`
-	Sex  string `info:"sex"`
+	Sex  string `info:"sex" tar:"brilliant!"`
 }
 
 func findTag(str interface{}) {
@@ -154,7 +156,8 @@ func findTag(str interface{}) {
 	for i := 0; i < t.NumField(); i++ {
 		tagInfo := t.Field(i).Tag.Get("info")
 		tagDoc := t.Field(i).Tag.Get("doc")
-		fmt.Println("info:", tagInfo, " doc:", tagDoc)
+		tagTar := t.Field(i).Tag.Get("tar")
+		fmt.Println("info:", tagInfo, " doc:", tagDoc, "tar:", tagTar)
 	}
 }
 
@@ -164,3 +167,37 @@ func TestTag(t *testing.T) {
 }
 
 //=============================================================================================
+
+type Movie struct {
+	Title string `json:"tit"`
+	Year int `json:"ye"`
+	Price int `json:"yuan"`
+	Actors []string `json:"actress"`
+}
+
+func TestJson(t *testing.T) {
+    movie := Movie{"happy ending", 2021, 9876, []string{"Zhang", "Cheng"}}
+    str, _ := json.Marshal(movie)
+    fmt.Println(movie, string(str))
+    myMovie := Movie{}
+    if err := json.Unmarshal(str, &myMovie); err != nil {
+    	fmt.Println(err)
+	}else {
+		fmt.Println(myMovie)
+	}
+}
+
+//=============================================================================================
+
+func TestGoroutine(t *testing.T) {
+    go func() {
+    	defer fmt.Println("A")
+
+    	func(){
+    		fmt.Println("B")
+			runtime.Goexit()
+		}()
+
+    	fmt.Println("a")
+	}()
+}
